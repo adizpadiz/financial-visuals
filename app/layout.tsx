e939bd31469283272064c0a1510c92aa7f22b168
adizpadiz <adam.svanberlithell@gmail.com>
2025-09-02 21:50:51 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Finance Visualizer",
+  description: "Visualize financial statements + simulator",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
